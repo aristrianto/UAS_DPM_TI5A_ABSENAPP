dbe2db3b754af154908979a3175eeece31a3ee96
aristrianto <email@kamu.com>
2022-01-20 15:21:40 +0700

    Aplikasi Smart Absen

---
+++ app/src/main/java/ac/id/atmaluhur/mhs/ABSENAPP/LembutKerja.java
@@ -0,0 +1,14 @@
+package ac.id.atmaluhur.mhs.ABSENAPP;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LembutKerja extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lembut_kerja);
+    }
+}
